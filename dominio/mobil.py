db40bf6bed1ed9fe6194cfea35f5cb07f66ba455
Juareca <alberjuan2411@gmail.com>
2026-07-01 21:58:59 -0500

    Eliminar carpeta dominio del repositorio público

--- dominio/mobil.py
+++
@@ -1,3 +0,0 @@
-class MOBIL:
-    def __init__(self, threshold=0.5):
-        self.threshold = threshold
--- dominio/semaforo.py
+++
@@ -1,7 +0,0 @@
-class Semaforo:
-    
-    def __init__(self, posicion: float):
-        self.posicion = posicion
-    
-        self.estado = "rojo"  # estado inicial
-        
